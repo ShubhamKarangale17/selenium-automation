748e53b3d543e628ed1a78217109474008622970
shubh17 GitHub <shubhamkarangale44@gmail.com>
2025-12-31 14:11:03 +0530

    Added LoginTest2 using POM

---
+++ src/tests/LoginTest2.java
@@ -0,0 +1,38 @@
+package tests;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest2 extends BaseTest {
+
+    public static void main(String[] args) {
+
+        // Create object of test class
+        LoginTest2 test = new LoginTest2();
+
+        // Setup browser
+        test.setUp();
+
+        // Open application
+        test.driver.get("https://www.saucedemo.com");
+
+        // Use LoginPage (POM)
+        LoginPage loginPage = new LoginPage(test.driver);
+
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
+
+        // Validation
+        String currentUrl = test.driver.getCurrentUrl();
+
+        if (currentUrl.contains("inventory")) {
+            System.out.println("LOGIN TEST 2 PASSED");
+        } else {
+            System.out.println("LOGIN TEST 2 FAILED");
+        }
+
+        // Close browser
+        test.tearDown();
+    }
+}
