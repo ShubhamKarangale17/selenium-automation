55b6bc8f602159002b568e3e502e5dd3008ccaea
Shubham Karangale <shubhamkarangale44@gmail.com>
2026-01-02 02:52:04 +0530

    allows to Take Screenshot When Test Fails

---
+++ src/tests/LoginNegativeTestScreenshot.java
@@ -0,0 +1,38 @@
+package tests;
+
+import base.BaseTest;
+import pages.LoginPage;
+import utils.ScreenshotUtil;
+
+public class LoginNegativeTestScreenshot extends BaseTest {
+
+    public static void main(String[] args) {
+
+        LoginNegativeTest test = new LoginNegativeTest();
+        test.setUp();
+
+        test.driver.get("https://www.saucedemo.com");
+
+        LoginPage loginPage = new LoginPage(test.driver);
+
+        // Invalid credentials
+        loginPage.enterUsername("wrong_user");
+        loginPage.enterPassword("wrong_password");
+        loginPage.clickLogin();
+
+        // Validation
+        String errorText = loginPage.getErrorMessage();
+
+        boolean isErrorDisplayedCorrectly =
+                errorText.contains("Username and password do not match");
+
+        if (isErrorDisplayedCorrectly) {
+            System.out.println("NEGATIVE LOGIN TEST PASSED");
+        } else {
+            System.out.println("NEGATIVE LOGIN TEST FAILED");
+            ScreenshotUtil.takeScreenshot(test.driver, "NegativeLoginFailure");
+        }
+
+        test.tearDown();
+    }
+}
