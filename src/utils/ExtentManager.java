7cde0a4be14525a421615e786341dd368ddaa41f
Shubham Karangale <shubhamkarangale44@gmail.com>
2026-01-05 18:42:20 +0530

    Integrated TEST Reports

---
+++ src/utils/ExtentManager.java
@@ -0,0 +1,24 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getExtentReport() {
+        if (extent == null) {
+
+            ExtentSparkReporter reporter =
+                    new ExtentSparkReporter("reports/ExtentReport.html");
+
+            reporter.config().setReportName("Automation Test Report");
+            reporter.config().setDocumentTitle("QA Automation Results");
+
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+        }
+        return extent;
+    }
+}
